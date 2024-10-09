effd288626c58e55e76a286ab0de8cc640c67662
Ly Braust <ly.braust@tktk.ee>
2024-10-09 18:42:26 +0300

    add week1SQL code

---
+++ week13SQL/Program.cs
@@ -0,0 +1,131 @@
+﻿
+using System.Data;
+using System.Data.SQLite;
+
+
+ReadData(CreateConnection());
+//InsertCustomer(CreateConnection());
+// RemoveCustomer(CreateConnection());
+FindCustomer(CreateConnection());
+
+static SQLiteConnection CreateConnection()
+{
+    SQLiteConnection connection = new SQLiteConnection("Data Source=mydb.db; Version=3; New =True; Compress = True;");
+    try
+    {
+        connection.Open();
+        Console.WriteLine("DB found.");
+    }
+    catch
+    {
+        Console.WriteLine("DB not found.");
+    }
+
+    return connection;
+}
+
+static void ReadData(SQLiteConnection myConnection)
+{
+    Console.Clear();
+    SQLiteDataReader reader;
+    SQLiteCommand command;
+
+    
+    //command.CommandText = "SELECT customer.firstName, customer.lastName, status.statusType from customerStatus " +
+    //"JOIN customer on customer.rowid = customerStatus.customerId " +
+    //"JOIN status on status.rowid = customerStatus.statusId " +
+    //"ORDER BY status.statustype";
+
+    command = myConnection.CreateCommand();
+    command.CommandText = "SELECT rowid, * From customer";
+
+    reader = command.ExecuteReader();
+
+    while (reader.Read())
+    {
+        string readerRowId = reader["rowid"].ToString();
+        string readerStringFirstName = reader.GetString(1);
+        string readerStringLastName = reader.GetString(2);
+        string readerStringDoB = reader.GetString(3);
+
+        Console.WriteLine($"{readerRowId}. Full name: {readerStringFirstName} {readerStringLastName}; DoB: {readerStringDoB}");
+    }
+
+    myConnection.Close();
+}
+
+static void InsertCustomer(SQLiteConnection myConnection)
+{
+    SQLiteCommand command;
+    string fName, lName, dob;
+
+    Console.WriteLine("Enter first name:");
+    fName = Console.ReadLine();
+    Console.WriteLine("Enter last name:");
+    lName = Console.ReadLine();
+    Console.WriteLine("Enter date of birth (mm-dd-yy):");
+    dob = Console.ReadLine();
+
+    command = myConnection.CreateCommand();
+    command.CommandText = $"INSERT INTO customer(firstName, lastName, dateOfBirth) " +
+    $"VALUES ('{fName}', '{lName}', '{dob}')";
+
+    int rowInserted = command.ExecuteNonQuery();
+    Console.WriteLine($"Row inserted: {rowInserted}");
+
+    ReadData(myConnection);
+
+    myConnection.Close();
+}
+
+static void RemoveCustomer(SQLiteConnection myConnection)
+{
+    SQLiteCommand command;
+
+    string idToDelete;
+    Console.WriteLine("Enter an id to delete a customer:");
+    idToDelete = Console.ReadLine();
+
+    command = myConnection.CreateCommand();
+    command.CommandText = $"DELETE FROM customer WHERE rowid = {idToDelete}";
+    int rowRemoved = command.ExecuteNonQuery();
+
+    Console.WriteLine($"{rowRemoved} was removed from the table customer.");
+
+    ReadData(myConnection);
+    myConnection.Close();
+
+}
+
+static void FindCustomer(SQLiteConnection myConnection)
+{
+    SQLiteDataReader reader;
+    SQLiteCommand command;
+    string searchName;
+    Console.WriteLine("Enter a first name to display customer data:");
+
+    searchName = Console.ReadLine();
+    command = myConnection.CreateCommand();
+    command.CommandText = $"SELECT customer.rowid, customer.firstname, customer.lastname, status.statusType " + // Muudetud veeru nimi
+        $"FROM customerStatus " +
+        $"JOIN customer ON customer.rowid = customerStatus.customerId " +
+        $"JOIN status ON status.rowid = customerStatus.statusId " +
+        $"WHERE customer.firstname LIKE @searchName"; // Lisatud 'customer.' ette
+
+    // Lisame parameetri päringule
+    command.Parameters.AddWithValue("@searchName", "%" + searchName + "%"); // Parameetri lisamine õigesti
+
+    reader = command.ExecuteReader();
+
+    while (reader.Read())
+    {
+        string readerRowid = reader["rowid"].ToString();
+        string readerStringName = reader.GetString("firstname");
+        string readerStringLastName = reader.GetString("lastname");
+        string readerStringStatus = reader.GetString("statusType"); // Muudetud veeru nimi
+        Console.WriteLine($"Search result: ID: {readerRowid}. {readerStringName} {readerStringLastName}. Status: {readerStringStatus}");
+    }
+
+    reader.Close();
+}
+
